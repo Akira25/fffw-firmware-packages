4dc6a0279b2df498f7491622fae65d7029315d1d
Martin Hübner <martin.hubner@web.de>
2020-04-03 21:06:56 +0200

    add package autoupdate for fuerstenwalde

---
+++ utils/freifunk-berlin-autoupdate/files/config/config_defaults.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+
+#if autoupdate is not present in crontab, include it.
+crontab -l | grep /usr/bin/autoupdate >>/dev/null
+if [ $? != 0 ]; then
+    #get a fairly random update-time, to protect the servers from DoS
+    HOUR=$(($(tr -cd 0-9 </dev/urandom | head -c 2) % 24))
+    MIN=$(($(tr -cd 0-9 </dev/urandom | head -c 2) % 60))
+    echo "$MIN $HOUR * * *        /usr/bin/autoupdate -a" >>/etc/crontabs/root
+    echo "$MIN $HOUR * * *        /usr/bin/autoupdate -s" >>/etc/crontabs/root
+    /etc/init.d/cron restart
+fi
+
+# check, if sysfixtime has done its job right
+find /etc/time.reminder
+if [ $? == 0 ]; then
+    NEW_DATE=$(date -r /etc/time.reminder +%s)
+    ACT_DATE=$(date +%s)
+    if [ ACT_DATE -lt NEW_DATE ]; then
+        exit 1
+    fi
+else
+    exit 1
+fi
+
+#set LAST_UPGR to $TODAY. /etc/init.d/sysfixtime should alreay have set the correct date.
+if [ -e /etc/config/autoupdate ]; then
+    TODAY=$(date +%s)
+    uci set autoupdate.internal.last_upgr="$TODAY"
+    uci commit autoupdate
+else
+    exit 1
+fi
+
+exit 0
