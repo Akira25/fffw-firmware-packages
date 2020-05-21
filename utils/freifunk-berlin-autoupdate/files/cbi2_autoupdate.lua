f1d7f1a9b7c00390e00787d6dcefaa90e3835ed0
Martin Hübner <martin.hubner@web.de>
2020-05-21 02:33:16 +0200

    Makefile: remove time.reminder.

---
+++ utils/freifunk-berlin-autoupdate/files/cbi2_autoupdate.lua
@@ -0,0 +1,34 @@
+map= Map("autoupdate", "autoupdate") -- We want to edit the uci config file /etc/config/autoupdate
+
+sec_auto = map:section(NamedSection, "automode", "settings", "Atomatische Updates")
+c = sec_auto:option(ListValue, "automode", "automode", "Updates selbstständig durchfühern?")
+c:value("true", "ja") -- Key and value pairs
+c:value("false", "nein")
+c.default = "false"
+d = sec_auto:option(ListValue, "branch", "Branch")
+d:value("release", "release")
+d:value("testing", "testing")
+d:value("development", "development")
+d:value("costum", "--custom--")
+g = sec_auto:option(Value, "branch2", "Branch2"):depends("branch", "costum")
+--if d ~= "costum" then
+--	g = d
+--end
+
+
+sec_router = map:section(NamedSection, "router", "settings", "Router", "Hier sollte nur etwas geändert werden, wenn automatische Updates nicht funktionieren. Der Standardwert ist 'auto' für automatische Erkennung.")
+a = sec_router:option(Value, "model", "Routermodell", "z.B.: TP-LINK TL-WR1043ND v2")
+a.default = "auto"
+b = sec_router:option(ListValue, "type", "Uplink-Type")
+b:value("default", "default - direkt ausleiten")
+b:value("tunneldigger", "tunneldigger - über VPN ausleiten")
+b:value("auto", "automatisch erkennen")
+b.default = "auto"
+
+sec_internal = map:section(NamedSection, "internal", "settings", "Interne Einstellungen")
+d = sec_internal:option(Value, "json_link_server", "Link-Listen", "URL, unter der nach Link-Listen gesucht wird.")
+d.default = "null"
+e = sec_internal:option(Value, "minimum_signatures", "Anzahl Signaturen", "Mindestanzahl gültiger Signaturen, um ein Update durchführen zu können.")
+e.default = "4"
+
+return map -- Returns the map
